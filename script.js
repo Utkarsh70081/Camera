6addb890deee7037dab756832abada7b95a46c57
Utkarsh_Mishra <97652158+Utkarsh70081@users.noreply.github.com>
2024-02-02 18:12:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const nightOutButton = document.getElementById('nightOutButton');
+//     const animatedBox = document.querySelector('.box');
+
+//     // Check if the nightOut button is initially checked
+//     if (nightOutButton.checked) {
+//         animatedBox.classList.add('nightOutAnimation');
+//     }
+
+//     // Add event listener to the nightOut button
+//     nightOutButton.addEventListener('change', function () {
+//         if (this.checked) {
+//             animatedBox.classList.add('nightOutAnimation');
+//         } else {
+//             animatedBox.classList.remove('nightOutAnimation');
+//         }
+//     });
+// });
